7e83f203af6bc1ed3a6fe46e2262be4e19fb2c2a
Daniel Ahn <iamdanahn@gmail.com>
2020-12-16 14:44:37 -0500

    Added twoSum

--- skeleton 5/phase_1_arrays.js
+++ skeleton 5/phase_1_arrays.js
@@ -8,3 +8,18 @@ Array.prototype.uniq = function() {
     return x;
 }
 
+// why does i2 (j) work?
+
+Array.prototype.twoSum = function() {
+    let x = []
+    for (let i = 0; i < this.length; i++ ) {
+        for (let j = 0 ; j < this.length; j++) {
+            if ((j > i) && (this[i] + this[j] === 0)) {
+                x.push(this[i]);
+                x.push(this[j]);
+            }
+        }
+    }
+    console.log(x);
+}
+
