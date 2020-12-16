c522c67063fe3cdb2371c34ae9a6f7d5de90e2c8
henryzihaohuang <atlas.bb.contact@gmail.com>
2020-12-16 14:24:45 -0600

    Finish phase 1

--- skeleton 5/phase_1_arrays.js
+++ skeleton 5/phase_1_arrays.js
@@ -13,13 +13,22 @@ Array.prototype.uniq = function() {
 Array.prototype.twoSum = function() {
     let x = []
     for (let i = 0; i < this.length; i++ ) {
-        for (let j = 0 ; j < this.length; j++) {
-            if ((j > i) && (this[i] + this[j] === 0)) {
-                x.push(this[i]);
-                x.push(this[j]);
+        for (let i2 = 0 ; i2 < this.length; i2++) {
+            if ((i2 > i) && (this[i] + this[i2] === 0)) {
+                x.push([i,i2]);
             }
         }
     }
     console.log(x);
 }
 
+Array.prototype.transpose = function() {
+    let newArray = Array(3).fill(0).map(x => Array(3));
+
+    for (let i = 0; i <this.length; i++ ) {
+        for (let j = 0; j < this.length; j++ ) {
+            newArray[i].push(this[j][i]) ;
+        }
+    }
+    console.log(newArray);
+}
